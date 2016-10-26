f7d77301715114e71ea24254dcbfe557e264f798
wanghao <117663444@qq.com>
2016-10-26 16:43:49 +0800

    数据

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+var dataSource = {
+  message: 'hello'
+}
+
 var vm = new Vue({
-  el: '#app'
+  el: '#app',
+  data: dataSource
+})
+
+vm.$watch('message', function (newValue, oldValue) {
+  console.log(newValue, oldValue)
 })
