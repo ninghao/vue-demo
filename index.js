20f936d2fdcb2405e544733bedbfe620d9f20ad6
wanghao <117663444@qq.com>
2016-10-29 14:05:51 +0800

    键盘事件

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 var dataSource = {
-  counter: 0
+
 }
 
 var vm = new Vue({
   el: '#app',
   data: dataSource,
   methods: {
-    like (event) {
-      this.counter += 1
-      console.log(event)
+    process (event) {
+      console.log(event.target.value)
     }
   }
 })
