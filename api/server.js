8012bb72a66c1295bacf4f8b0521dc71e159b604
wanghao8080 <117663444@qq.com>
2017-08-26 11:11:31 +0800

    API

---
+++ api/server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const router = express.Router()
+
+app.use(bodyParser.json())
+
+let count = [1, 2, 3]
+
+router.route('/count')
+  .get((request, response) => {
+    response.send({
+      count
+    })
+  })
+  .post((request, response) => {
+    count.push(Number(request.body.number))
+    response.status(201).send({
+      message: 'ok'
+    })
+  })
+  .delete((request, response) => {
+    count.pop()
+    response.status(200).send({
+      message: 'ok'
+    })
+  })
+
+app.use('/api', router)
+
+app.listen(8080, () => {
+  console.log('http://localhost:8080')
+})
+
+
+
+//
